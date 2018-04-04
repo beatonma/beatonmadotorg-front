5647c19e3799c4cf037c2358cc0178ee9a5e63ec
Michael Beaton <beatonma@gmail.com>
2018-04-04 13:01:19 +0100

    Removed jquery

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,18 @@
 function initScrollTransitions() {
-    console.log("initScrollTransitions()");
-    $('main').scroll(function() {
-        const main_header = $('#beatonma_header');
-        const article_header = $('#article_header');
-        
-        if (!main_header.length || !article_header.length) {
+    document.getElementsByTagName('main')[0].addEventListener('scroll', () => {
+        console.log('scrolling');
+        const main_header = document.getElementById('beatonma_header');
+        const article_header = document.getElementById('article_header');
+
+        if (!main_header || !article_header) {
             return;
         }
 
-        let main_bottom = main_header.outerHeight();
-        let article_bottom = article_header.offset().top + article_header.outerHeight();
+        let main_bottom = main_header.offsetHeight;
+        let article_bottom = article_header.getBoundingClientRect().top + article_header.offsetHeight;
 
         if (article_bottom <= main_bottom) {
-            applyAccent()
+            applyAccent();
         }
         else {
             removeAccent();
@@ -22,7 +22,7 @@ function initScrollTransitions() {
 
 function init() {
     initPageTransitions();
-    $(document).ready(function() {
+    document.addEventListener('DOMContentLoaded', () => {
         initScrollTransitions();
         initSearch();
     });
@@ -30,23 +30,23 @@ function init() {
 
 function initSearch() {
     // Replace the default form submission so we can animate the page change
-    const searchform = document.querySelector('#searchform');
-    searchform.addEventListener("submit", function(ev) {
-        const searchform = $(this);
-        let searchUrl = '/search?' + searchform.serialize();
+    const searchform = document.getElementById('searchform');
+    searchform.addEventListener('submit', () => {
+        const searchform = document.getElementById('searchform');
+        const searchUrl = '/search?' + new URLSearchParams(new FormData(formElement)).toString();
         history.pushState(null, null, searchUrl);
         changePage('url=' + searchUrl);
         ev.preventDefault();
     });
     
     // We want the search box to expand to a new line when on a small device
-    // so we need to listen to focus changes and toggle the 'search_container'
+    // so we need to listen to focus changes and toggle the 'search-container'
     // class on the parent form
-    const searchbox = document.querySelector('#search');
-    searchbox.addEventListener('focus', function(ev) {
-        $('#searchform').addClass('search_container');
-    });
-    searchbox.addEventListener('focusout', function(ev) {
-        $('#searchform').removeClass('search_container');
-    });
+    // const searchbox = document.getElementById('search');
+    // searchbox.addEventListener('focus', function(ev) {
+    //     document.getElementById('searchform').classList.add('search-container');
+    // });
+    // searchbox.addEventListener('focusout', function(ev) {
+    //     document.getElementById('searchform').classList.remove('search-container');
+    // });
 }
