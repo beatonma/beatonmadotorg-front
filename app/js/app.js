8c56fcc4534948be9ec7b6f755c36ac489a89ee2
Michael Beaton <beatonma@gmail.com>
2018-04-16 14:25:54 +0100

    Updated search event listeners

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,47 @@
 const app = (() => {
-function initScrollTransitions() {
-    document.getElementsByTagName('main')[0].addEventListener('scroll', () => {
-        const main_header = document.getElementById('beatonma_header');
-        const article_header = document.getElementById('article_header');
+// function initScrollTransitions() {
+//     document.getElementsByTagName('main')[0].addEventListener('scroll', () => {
+//         const main_header = document.getElementById('beatonma_header');
+//         const article_header = document.getElementById('article_header');
 
-        if (!main_header || !article_header) {
-            return;
-        }
+//         if (!main_header || !article_header) {
+//             return;
+//         }
 
-        let main_bottom = main_header.offsetHeight;
-        let article_bottom = article_header.getBoundingClientRect().top + article_header.offsetHeight;
+//         let main_bottom = main_header.offsetHeight;
+//         let article_bottom = article_header.getBoundingClientRect().top + article_header.offsetHeight;
 
-        if (article_bottom <= main_bottom) {
-            applyAccent();
-        }
-        else {
-            removeAccent();
-        }
-    });
-}
+//         if (article_bottom <= main_bottom) {
+//             theme.apply();
+//         }
+//         else {
+//             theme.unapply();
+//         }
+//     });
+// }
 
 function init() {
     initPageTransitions();
-    document.addEventListener('DOMContentLoaded', () => {
-        initScrollTransitions();
-        initSearch();
-    });
+    initSearch();
 }
 
 function initSearch() {
     // Replace the default form submission so we can animate the page change
     const searchform = document.getElementById('searchform');
-    searchform.addEventListener('submit', () => {
+    searchform.addEventListener('submit', (ev) => {
+        ev.preventDefault();
         const searchform = document.getElementById('searchform');
-        const searchUrl = '/search?' + new URLSearchParams(new FormData(formElement)).toString();
+        const searchUrl = '/search?' + new URLSearchParams(
+            new FormData(searchform)).toString();
         history.pushState(null, null, searchUrl);
         changePage('url=' + searchUrl);
-        ev.preventDefault();
+    });
+
+    const searchIcon = document.getElementById('search_icon');
+    searchIcon.addEventListener('focus', () => {
+        // Move focus to search box when icon is selected
+        const searchInput = document.getElementById('search');
+        searchInput.focus();
     });
     
     // We want the search box to expand to a new line when on a small device
