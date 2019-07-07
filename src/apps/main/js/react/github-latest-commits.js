7babb25e54603c2160d46d06630d69f88d1adbd0
Michael Beaton <beatonma@gmail.com>
2019-07-07 20:28:35 +0100

    First experiment with React constructing views for recent github commits

---
+++ src/apps/main/js/react/github-latest-commits.js
@@ -0,0 +1,71 @@
+function Commit(props) {
+    function renderCommitMessages(changes) {
+        return changes.map((change) =>
+            <GitCommitMessage key={change.sha} message={change.message} />
+        );
+    }
+
+    return (
+        <div className="github-recent-commit">
+            <a href={"" + props.commit.repo.url} className="github-recent-repo">{props.commit.repo.name}</a>
+            {renderCommitMessages(props.commit.changes)}
+        </div>
+    );
+}
+
+class GitCommitMessage extends React.Component {
+    render() {
+        return (
+            <div className="github-commit-message">
+                {this.props.message}
+            </div>
+        )
+    }
+}
+
+class GithubLatestCommits extends React.Component {
+    renderCommits(commits) {
+        return commits.map(c => 
+            <Commit key={c.created_at} commit={c} />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderCommits(this.props.commits)}
+            </div>
+        )
+    }
+}
+
+const commits = (() => {
+
+    function getRecentGithubCommits() {
+        const url = '/api/github_latest/';
+        fetch(url, {
+            method: 'GET',
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            if (!data['commits']) {
+                return;
+            }
+            buildViews(data['commits']);
+        }).catch((err) => {
+            console.log('Error getting recent commits:' + err);
+        })
+    }
+
+    function buildViews(commits) {
+        ReactDOM.render(<GithubLatestCommits commits={commits} />, document.getElementById('github_recent'));
+    }
+
+    if (document.getElementById('github_recent')) {
+        getRecentGithubCommits();
+    }
+
+    return {
+        'load': getRecentGithubCommits
+    }
+})();
