0182a03b7871b9c66c22f8466283472e276c5276
Michael Beaton <beatonma@gmail.com>
2019-07-13 22:27:36 +0100

    Started to implement display of private repository commit metadata

--- src/apps/main/js/react/github-latest-commits.js
+++ src/apps/main/js/react/github-latest-commits.js
@@ -1,5 +1,28 @@
 const commits = (() => {
-    function Commit(props) {
+    function GithubLatestCommits(props) {
+        function renderCommits(commits) {
+            return commits.map(c => 
+                <PublicCommit
+                    key={c.created_at}
+                    commit={c} />
+            );
+        }
+
+        return (
+            <div className="card-content">
+                <h3>Commits:</h3>
+                {renderCommits(props.commits)}
+            </div>
+        )
+    }
+
+    function Timestamp(props) {
+        return <time class="dt-updated label" datetime={"" + new Date(props.timestamp)}>
+            {formatDate(new Date(props.timestamp))}
+        </time>
+    }
+
+    function PublicCommit(props) {
         function renderCommitMessages(changes) {
             return changes.map((change) =>
                 <GitCommitMessage
@@ -8,45 +31,37 @@ const commits = (() => {
             );
         }
 
+        function renderTimestamp(commit) {
+            if (formatDate(new Date(commit.start)) == formatDate(new Date(commit.end))) {
+                return <Timestamp timestamp={commit.start} />
+            }
+            else {
+                return <span>
+                    <Timestamp timestamp={commit.start} /> - <Timestamp timestamp={commit.end} />
+                </span>
+            }
+        }
+
         return (
             <div className="github-recent-commit">
                 <a href={"" + props.commit.repo.url} className="github-recent-repo">
                     {props.commit.repo.name}
-                </a> <time class="dt-updated label" datetime={"" + new Date(props.commit.created_at)}>
-                    {formatDate(new Date(props.commit.created_at))}
-                </time>
+                </a> {renderTimestamp(props.commit)}
                 {renderCommitMessages(props.commit.changes)}
             </div>
         );
     }
 
-    class GitCommitMessage extends React.Component {
-        render() {
-            return (
-                <div className="github-commit-message">
-                    - {this.props.message}
-                </div>
-            )
-        }
+    function PrivateCommitGroup(props) {
+
     }
 
-    class GithubLatestCommits extends React.Component {
-        renderCommits(commits) {
-            return commits.map(c => 
-                <Commit
-                    key={c.created_at}
-                    commit={c} />
-            );
-        }
-
-        render() {
-            return (
-                <div className="card-content">
-                    <h3>Commits:</h3>
-                    {this.renderCommits(this.props.commits)}
-                </div>
-            )
-        }
+    function GitCommitMessage(props) {
+        return (
+            <div className="github-commit-message">
+                - {props.message}
+            </div>
+        );
     }
 
     function getRecentGithubCommits() {
@@ -59,10 +74,58 @@ const commits = (() => {
             if (!data['commits']) {
                 return;
             }
-            buildViews(data['commits']);
+            const commits = compressCommits(data['commits']);
+            buildViews(commits);
         }).catch((err) => {
             console.log('Error getting recent commits:' + err);
         })
+    }
+
+    function compressCommits(commits) {
+        let compressed = [];
+
+        // Group consecutive commits to same repo
+        let bucket = {
+            name: null,
+            repo: null,
+            start: null,
+            end: null,
+            changes: [],
+            changeCount: 0,
+            public: null
+        };
+
+        commits.forEach((c) => {
+            if (bucket.name === c.repo.name) {
+                // Update existing bucket
+                bucket.start = c.created_at;
+                bucket.changes.push(...c.changes);
+                bucket.changeCount += c.changeCount;
+            }
+            else {
+                // Store old bucket
+                if (bucket.name != null) {
+                    compressed.push(bucket);
+                }
+
+                bucket = {
+                    name: c.repo.name,
+                    repo: c.repo,
+                    start: c.created_at,
+                    end: c.created_at,
+                    changes: c.changes,
+                    change_count: c.change_count,
+                    public: c.public
+                };
+            }
+        });
+        if (bucket.name != null) {
+            compressed.push(bucket);
+        }
+
+        // TODO combine private repos and show commit count
+
+        return compressed;
     }
 
     function buildViews(commits) {
