05d9f1498df5e613a7b92ffdc79393306d36970a
Michael Beaton <beatonma@gmail.com>
2022-01-24 11:37:02 +0000

    Removed unused Events

--- src/apps/main/js/events.js
+++
@@ -1,9 +0,0 @@
-// const events = (() => {
-// export const onPageChangeEvent = new Event("onPageChange");
-// export const onPageExitEvent = new Event("onPageExit");
-
-// return {
-//     onPageChange: onPageChangeEvent,
-//     onPageExit: onPageExitEvent
-// }
-// })();
