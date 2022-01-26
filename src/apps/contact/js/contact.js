012d84c57f9441a09b20b63b5b3597fe5eecf101
Michael Beaton <beatonma@gmail.com>
2022-01-26 15:40:35 +0000

    Fixed recaptcha callback. Moved success message into main ui card. Formatting.

--- src/apps/contact/js/contact.js
+++ src/apps/contact/js/contact.js
@@ -1,45 +1,44 @@
+import { getCsrfToken } from "../../main/js/util.js";
+
 function showSuccess() {
-  document.getElementById("contact_form").classList.add("hidden");
-  document.getElementById("success_message").classList.remove("hidden");
+    document.getElementById("contact_form").classList.add("hidden");
+    document.getElementById("success_message").classList.remove("hidden");
 }
 
 function showFailure(response) {
-  document.getElementById("failure_message").classList.remove("hidden");
-  console.error("[" + response.status + "]: " + response.statusText);
+    document.getElementById("failure_message").classList.remove("hidden");
+    console.error(`[${response.status}]: ${response.statusText}`);
 }
 
 export function onSubmitContact(recaptchaToken) {
-  console.log("onSubmit" + recaptchaToken);
-  const contact = document.getElementById("contact_form");
-  document.getElementById("beatonma_loading").classList.add("loading");
+    const contact = document.getElementById("contact_form");
+    document.getElementById("beatonma_loading").classList.add("loading");
 
-  const form = new FormData(contact);
-  const csrftoken = getCookie("csrftoken");
-  const headers = new Headers();
-  headers.append("X-CSRFToken", csrftoken);
-  headers.append("g-recaptcha-response", recaptchaToken);
+    const form = new FormData(contact);
+    const csrftoken = getCsrfToken();
+    const headers = new Headers();
+    headers.append("X-CSRFToken", csrftoken);
 
-  fetch("/contact/send", {
-    headers: headers,
-    method: "POST",
-    body: form,
-    credentials: "same-origin",
-  })
-    .then(response => {
-      document.getElementById("beatonma_loading").classList.remove("loading");
-      if (response.ok) {
-        showSuccess();
-      } else {
-        showFailure(response);
-      }
+    fetch("/contact/send", {
+        headers: headers,
+        method: "POST",
+        body: form,
+        credentials: "same-origin",
     })
-    .catch(err => {
-      console.error(err);
-      showFailure();
-    });
+        .then(response => {
+            document
+                .getElementById("beatonma_loading")
+                .classList.remove("loading");
+            if (response.ok) {
+                showSuccess();
+            } else {
+                showFailure(response);
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            showFailure();
+        });
 }
 
-window.contact = {
-  hello: "HELLO :)",
-  onSubmit: onSubmitContact,
-};
+window.onSubmitContact = onSubmitContact;
