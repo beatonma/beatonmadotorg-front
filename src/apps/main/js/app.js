721ea01ffe8c49475f5a33c42ca3544c3a968150
Michael Beaton <beatonma@gmail.com>
2022-05-03 14:16:12 +0100

    Deleted old github-latest-commits.jsx implementation.

--- src/apps/main/js/app.js
+++ src/apps/main/js/app.js
@@ -3,6 +3,5 @@ import "./page-transitions.js";
 import "./search.js";
 import "./theme.js";
 import "./get-webmentions";
-import "./github-latest-commits";
 import "./load-media";
 import "./apps.js";
