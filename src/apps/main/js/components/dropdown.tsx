1e7bd054114de7f6d61c4032eadc228530314679
Michael Beaton <beatonma@gmail.com>
2022-05-14 14:49:58 +0100

    <Dropdown /> now conforms to WAI guidelines: https://www.w3.org/TR/wai-aria-practices-1.1/#disclosure
    LargeFeedItem now uses Dropdown.

--- src/apps/main/js/components/dropdown.tsx
+++ src/apps/main/js/components/dropdown.tsx
@@ -1,16 +1,32 @@
-import React, { HTMLAttributes, useState } from "react";
+import React, { HTMLAttributes, KeyboardEvent, useId, useState } from "react";
 import { MaterialIcon } from "./icons";
 import { joinClassNames } from "./props";
 
 interface DropdownProps extends HTMLAttributes<any> {
     expandedDefault?: boolean;
     header: string | React.ReactNode;
+    onToggle?: (expanded: boolean) => void;
 }
 export function Dropdown(props: DropdownProps) {
-    const { expandedDefault, header, className, children, ...rest } = props;
+    const { expandedDefault, header, onToggle, className, children, ...rest } =
+        props;
 
     const [expanded, setExpanded] = useState(expandedDefault || false);
-    const toggle = () => setExpanded(!expanded);
+    const contentID = useId();
+
+    const toggle = () => {
+        const value = !expanded;
+        setExpanded(value);
+        onToggle?.(value);
+    };
+
+    const onKeyDown = (event: KeyboardEvent) => {
+        console.log(event.key);
+        if (event.code === "Enter" || event.code === "Space") {
+            event.preventDefault();
+            toggle();
+        }
+    };
 
     return (
         <div
@@ -18,8 +34,16 @@ export function Dropdown(props: DropdownProps) {
             data-expanded={expanded}
             {...rest}
         >
-            <div className="row dropdown-header" onClick={toggle}>
-                <div className="dropdown-title">{title}</div>
+            <div
+                className="dropdown-header"
+                onClick={toggle}
+                onKeyDown={onKeyDown}
+                aria-expanded={expanded}
+                aria-controls={contentID}
+                role="button"
+                tabIndex={0}
+            >
+                <div className="dropdown-title">{header}</div>
                 <MaterialIcon
                     className="dropdown-icon"
                     data-expanded={expanded}
@@ -28,7 +52,11 @@ export function Dropdown(props: DropdownProps) {
                 </MaterialIcon>
             </div>
 
-            <div className="dropdown-content" data-expanded={expanded}>
+            <div
+                id={contentID}
+                className="dropdown-content"
+                data-expanded={expanded}
+            >
                 {children}
             </div>
         </div>
--- src/apps/main/js/components/feed-item.tsx
+++ src/apps/main/js/components/feed-item.tsx
@@ -1,6 +1,6 @@
-import React, { HTMLAttributes, useEffect, useState } from "react";
-import { classNames } from "./props";
-import { MaterialIcon } from "./icons";
+import React, { HTMLAttributes } from "react";
+import { joinClassNames } from "./props";
+import { Dropdown } from "./dropdown";
 
 interface LargeFeedItemProps extends HTMLAttributes<any> {
     expandedDefault?: boolean;
@@ -8,43 +8,19 @@ interface LargeFeedItemProps extends HTMLAttributes<any> {
     parentID: string;
 }
 export function LargeFeedItem(props: LargeFeedItemProps) {
-    const [expanded, setExpanded] = useState<boolean>(
-        props.expandedDefault || true
-    );
+    const { className, expandedDefault, parentID, ...rest } = props;
 
-    const toggle = () => {
-        const value = !expanded;
-        setExpanded(value);
-        const container = document.querySelector(props.parentID) as HTMLElement;
-        container.dataset.expanded = `${value}`;
+    const onToggle = (expanded: boolean) => {
+        const container = document.querySelector(parentID) as HTMLElement;
+        container.dataset.expanded = `${expanded}`;
     };
 
-    useEffect(() => {
-        const container = document.querySelector(props.parentID) as HTMLElement;
-        container.dataset.expanded = `${expanded}`;
-    }, []);
-
     return (
-        <div
-            className={classNames(props, "feed-item-dropdown")}
-            data-expanded={expanded}
-        >
-            <div className="feed-item-dropdown-header" onClick={toggle}>
-                <h5 className="feed-item-dropdown-title">{props.title}</h5>
-                <MaterialIcon
-                    className="feed-item-dropdown-icon"
-                    data-expanded={expanded}
-                >
-                    arrow_drop_down
-                </MaterialIcon>
-            </div>
-
-            <div
-                className="feed-item-dropdown-content"
-                data-expanded={expanded}
-            >
-                {props.children}
-            </div>
-        </div>
+        <Dropdown
+            className={joinClassNames(className, "feed-item-dropdown")}
+            onToggle={onToggle}
+            expandedDefault={expandedDefault ?? true}
+            {...rest}
+        />
     );
 }
