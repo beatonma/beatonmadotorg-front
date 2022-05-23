a92ba68e632bbd4b167bfaff76cd7b4360bb2d11
Michael Beaton <beatonma@gmail.com>
2022-05-23 20:39:27 +0100

    Reworked theming to respect `prefers-color-scheme`.

--- src/apps/main/js/theme.ts
+++ src/apps/main/js/theme.ts
@@ -1,86 +1,34 @@
-import { clearCookie, cookieExists, getCookie, setCookie } from "./util";
-
-const DAY = "day";
-const NIGHT = "night";
-const ID_STYLESHEET = "theme";
 const ID_THEME_BUTTON = "theme_icon";
-const COOKIE = "theme";
+const CLASS_DARK = "dark";
+const CLASS_LIGHT = "light";
+const SCHEME_MEDIA_QUERY = "(prefers-color-scheme: dark)";
 
-const DAY_HREF = "/static/css/colors-day.min.css";
-const NIGHT_HREF = "/static/css/colors-night.min.css";
+let isDark = window.matchMedia(SCHEME_MEDIA_QUERY).matches;
 
-let themeName = DAY; // values: 'day', 'night'
+const toggleTheme = () => {
+    isDark = !isDark;
+    document.documentElement.classList.toggle(CLASS_DARK);
+    document.documentElement.classList.toggle(CLASS_LIGHT);
+};
 
-// Automatically change basic color themes depending on time of day
-// If forceAuto then cookie values will be ignored
-export function setDayNightAutoTheme(forceAuto?: boolean) {
-    if (forceAuto) {
-        clearCookie(COOKIE);
-    } else if (cookieExists(COOKIE)) {
-        const t = getCookie(COOKIE);
-        setTheme(t);
-        return;
-    }
+const setDark = (dark: boolean) => {
+    isDark = dark;
+    document.documentElement.classList.add(dark ? CLASS_DARK : CLASS_LIGHT);
+    document.documentElement.classList.remove(dark ? CLASS_LIGHT : CLASS_DARK);
+};
 
-    const h = new Date().getHours();
-    if (h > 8 && h < 20) {
-        themeName = DAY;
-        setTheme(themeName);
-    } else {
-        themeName = NIGHT;
-        setTheme(themeName);
-    }
+export const isDarkTheme = (): boolean => isDark;
+
+try {
+    document.documentElement.classList.add(isDark ? CLASS_DARK : CLASS_LIGHT);
+
+    window
+        .matchMedia(SCHEME_MEDIA_QUERY)
+        .addEventListener("change", event => setDark(event.matches));
+
+    document
+        .getElementById(ID_THEME_BUTTON)
+        .addEventListener("click", toggleTheme);
+} catch (e) {
+    console.error(e);
 }
-
-function setTheme(newtheme: string) {
-    const stylesheet = document.getElementById(ID_STYLESHEET);
-    console.log(`Theme: ${newtheme}`);
-
-    if (newtheme === DAY) {
-        stylesheet.setAttribute("href", DAY_HREF);
-        themeName = newtheme;
-    } else if (newtheme === NIGHT) {
-        stylesheet.setAttribute("href", NIGHT_HREF);
-        themeName = newtheme;
-    } else if (newtheme === "") {
-        if (themeName === "") {
-            // This should never happen, but better include it just in case to avoid apocalypse
-            themeName = DAY;
-        }
-        setTheme(themeName);
-    } else {
-        if (themeName === DAY && newtheme !== "") {
-            document
-                .querySelector("meta[name=theme-color]")
-                .setAttribute("content", newtheme);
-        }
-    }
-}
-
-export function toggleTheme() {
-    if (themeName === DAY) {
-        themeName = NIGHT;
-    } else {
-        themeName = DAY;
-    }
-
-    setCookie(COOKIE, themeName, 24 * 60 * 60 * 1000 * 30); // Remember this theme selection for 30 days
-    setTheme(themeName);
-}
-
-export function initTheme() {
-    setDayNightAutoTheme();
-    try {
-        document
-            .getElementById(ID_THEME_BUTTON)
-            .addEventListener("click", toggleTheme);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-export function isDarkTheme() {
-    return themeName === NIGHT;
-}
-
-initTheme();
