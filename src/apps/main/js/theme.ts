6e94ce5f8026fc9efab56f54d5141511fb67b76c
Michael Beaton <beatonma@gmail.com>
2022-05-24 14:57:11 +0100

    Removed colors definitions in html head. Persist explicit theme preference in localStorage.

--- src/apps/main/js/theme.ts
+++ src/apps/main/js/theme.ts
@@ -2,29 +2,44 @@ const ID_THEME_BUTTON = "theme_icon";
 const CLASS_DARK = "dark";
 const CLASS_LIGHT = "light";
 const SCHEME_MEDIA_QUERY = "(prefers-color-scheme: dark)";
+const STORAGE_THEME = "darkTheme";
 
-let isDark = window.matchMedia(SCHEME_MEDIA_QUERY).matches;
+let isDark: boolean = null;
 
-const toggleTheme = () => {
-    isDark = !isDark;
-    document.documentElement.classList.toggle(CLASS_DARK);
-    document.documentElement.classList.toggle(CLASS_LIGHT);
+const loadTheme = () => {
+    const storedIsDark: boolean = JSON.parse(
+        window.localStorage.getItem(STORAGE_THEME)
+    );
+
+    if (storedIsDark === null) {
+        isDark = window.matchMedia(SCHEME_MEDIA_QUERY).matches;
+    } else {
+        isDark = storedIsDark;
+    }
+
+    document.documentElement.classList.add(isDark ? CLASS_DARK : CLASS_LIGHT);
 };
 
-const setDark = (dark: boolean) => {
+const toggleTheme = async () => setDark(!isDark);
+
+const setDark = async (dark: boolean, save: boolean = true) => {
     isDark = dark;
     document.documentElement.classList.add(dark ? CLASS_DARK : CLASS_LIGHT);
     document.documentElement.classList.remove(dark ? CLASS_LIGHT : CLASS_DARK);
+
+    if (save) {
+        window.localStorage.setItem(STORAGE_THEME, `${isDark}`);
+    }
 };
 
 export const isDarkTheme = (): boolean => isDark;
 
 try {
-    document.documentElement.classList.add(isDark ? CLASS_DARK : CLASS_LIGHT);
+    loadTheme();
 
     window
         .matchMedia(SCHEME_MEDIA_QUERY)
-        .addEventListener("change", event => setDark(event.matches));
+        .addEventListener("change", event => setDark(event.matches, false));
 
     document
         .getElementById(ID_THEME_BUTTON)
